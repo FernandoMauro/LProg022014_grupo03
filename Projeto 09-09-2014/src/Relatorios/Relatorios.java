a8f2116afa3b67493ee45e4928febb8fb7ef88e4
FernandoMauro <fernando.frkl@gmail.com>
2014-10-12 11:27:30 -0300

    Atualização de Relatórios

---
+++ Projeto 09-09-2014/src/Relatorios/Relatorios.java
@@ -0,0 +1,75 @@
+package Relatorios;
+
+import ConectaBanco.ConectaBanco;
+import java.util.HashMap;
+import javax.swing.JOptionPane;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JRResultSetDataSource;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.view.JasperViewer;
+
+public class Relatorios {
+
+    ConectaBanco conecta = new ConectaBanco();
+
+    public Relatorios() {
+        conecta.conexao();
+    }
+
+    public void imprimeRelTiposVeiculos() {
+        try {
+            conecta.executaSql("select * from tipos");
+            JRResultSetDataSource relatTipos = new JRResultSetDataSource(conecta.rs);
+            JasperPrint jpTipos = JasperFillManager.fillReport("src\\Relatorios\\tiposVeiculos.jasper", new HashMap(), relatTipos);
+            JasperViewer jv = new JasperViewer(jpTipos, false);
+            jv.setExtendedState(JasperViewer.MAXIMIZED_BOTH);
+            jv.setTitle("Relatório de Tipos de Veiculos");
+            jv.setVisible(true);
+        } catch (JRException es) {
+            JOptionPane.showMessageDialog(null, "Erro ao execultar o Relatório!\n ERRO :" + es);
+        }
+    }
+
+    public void imprimeRelCategoriasVeiculos() {
+        try {
+            conecta.executaSql("SELECT categorias.id, categorias.nome, tipos.nome AS tipo FROM categorias INNER JOIN tipos ON categorias.id_tipo = tipos.id");
+            JRResultSetDataSource relatTipos = new JRResultSetDataSource(conecta.rs);
+            JasperPrint jpTipos = JasperFillManager.fillReport("src\\Relatorios\\categoriasVeiculos.jasper", new HashMap(), relatTipos);
+            JasperViewer jv = new JasperViewer(jpTipos, false);
+            jv.setExtendedState(JasperViewer.MAXIMIZED_BOTH);
+            jv.setTitle("Relatório de Categorias de Veiculos");
+            jv.setVisible(true);
+        } catch (JRException es) {
+            JOptionPane.showMessageDialog(null, "Erro ao execultar o Relatório!\n ERRO :" + es);
+        }
+    }
+
+    public void imprimeRelClientes() {
+        try {
+            conecta.executaSql("SELECT clientes.id, clientes.nome, concat(substring(clientes.placa,1,3),'-',substring(clientes.placa,4,7)) as placa, tipos.nome as tipo, categorias.nome as categoria FROM clientes INNER JOIN tipos ON clientes.tipo = tipos.id INNER JOIN categorias ON clientes.categoria = categorias.id ");
+            JRResultSetDataSource relatTipos = new JRResultSetDataSource(conecta.rs);
+            JasperPrint jpTipos = JasperFillManager.fillReport("src\\Relatorios\\clientes.jasper", new HashMap(), relatTipos);
+            JasperViewer jv = new JasperViewer(jpTipos, false);
+            jv.setExtendedState(JasperViewer.MAXIMIZED_BOTH);
+            jv.setTitle("Relatório de Categorias de Veiculos");
+            jv.setVisible(true);
+        } catch (JRException es) {
+            JOptionPane.showMessageDialog(null, "Erro ao execultar o Relatório!\n ERRO :" + es);
+        }
+    }
+
+    public void imprimeTabelaPrecos() {
+        try {
+            conecta.executaSql("SELECT * FROM PRECOS");
+            JRResultSetDataSource relatTipos = new JRResultSetDataSource(conecta.rs);
+            JasperPrint jpTipos = JasperFillManager.fillReport("src\\Relatorios\\tabelaPrecos.jasper", new HashMap(), relatTipos);
+            JasperViewer jv = new JasperViewer(jpTipos, false);
+            jv.setExtendedState(JasperViewer.MAXIMIZED_BOTH);
+            jv.setTitle("Relatório de Categorias de Veiculos");
+            jv.setVisible(true);
+        } catch (JRException es) {
+            JOptionPane.showMessageDialog(null, "Erro ao execultar o Relatório!\n ERRO :" + es);
+        }
+    }
+}
--- Projeto 09-09-2014/src/Visao/FrmPrincipal.java
+++ Projeto 09-09-2014/src/Visao/FrmPrincipal.java
@@ -6,6 +6,7 @@
 package Visao;
 
 import ConectaBanco.ConectaBanco;
+import Relatorios.Relatorios;
 import java.util.HashMap;
 import javax.swing.JOptionPane;
 import net.sf.jasperreports.engine.JRException;
@@ -21,6 +22,7 @@ import net.sf.jasperreports.view.JasperViewer;
 public class FrmPrincipal extends javax.swing.JFrame {
 
     ConectaBanco conecta = new ConectaBanco();
+    Relatorios rel = new Relatorios();
 
     /**
      * Creates new form Principal
@@ -271,17 +273,7 @@ public class FrmPrincipal extends javax.swing.JFrame {
 
     private void jMenuItem6ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem6ActionPerformed
         // TODO add your handling code here:
-        try {
-            conecta.executaSql("select * from tipos");
-            JRResultSetDataSource relatTipos = new JRResultSetDataSource(conecta.rs);
-            JasperPrint jpTipos = JasperFillManager.fillReport("src\\Relatorios\\tiposVeiculos.jasper", new HashMap(), relatTipos);
-            JasperViewer jv = new JasperViewer(jpTipos, false);
-            jv.setExtendedState(JasperViewer.MAXIMIZED_BOTH);
-            jv.setTitle("Relatório de Tipos de Veiculos");
-            jv.setVisible(true);
-        } catch (JRException es) {
-            JOptionPane.showMessageDialog(null, "Erro ao execultar o Relatório!\n ERRO :" + es);
-        }
+        rel.imprimeRelTiposVeiculos();
     }//GEN-LAST:event_jMenuItem6ActionPerformed
 
     private void jMenu2MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jMenu2MouseClicked
@@ -292,47 +284,17 @@ public class FrmPrincipal extends javax.swing.JFrame {
 
     private void jMenuCategoriasActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuCategoriasActionPerformed
         // TODO add your handling code here:
-        try {
-            conecta.executaSql("SELECT categorias.id, categorias.nome, tipos.nome AS tipo FROM categorias INNER JOIN tipos ON categorias.id_tipo = tipos.id");
-            JRResultSetDataSource relatTipos = new JRResultSetDataSource(conecta.rs);
-            JasperPrint jpTipos = JasperFillManager.fillReport("src\\Relatorios\\categoriasVeiculos.jasper", new HashMap(), relatTipos);
-            JasperViewer jv = new JasperViewer(jpTipos, false);
-            jv.setExtendedState(JasperViewer.MAXIMIZED_BOTH);
-            jv.setTitle("Relatório de Categorias de Veiculos");
-            jv.setVisible(true);
-        } catch (JRException es) {
-            JOptionPane.showMessageDialog(null, "Erro ao execultar o Relatório!\n ERRO :" + es);
-        }
+        rel.imprimeRelCategoriasVeiculos();
     }//GEN-LAST:event_jMenuCategoriasActionPerformed
 
     private void jMenuRelClientesActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuRelClientesActionPerformed
         // TODO add your handling code here:
-        try {
-            conecta.executaSql("SELECT clientes.id, clientes.nome, concat(substring(clientes.placa,1,3),'-',substring(clientes.placa,4,7)) as placa, tipos.nome as tipo, categorias.nome as categoria FROM clientes INNER JOIN tipos ON clientes.tipo = tipos.id INNER JOIN categorias ON clientes.categoria = categorias.id ");
-            JRResultSetDataSource relatTipos = new JRResultSetDataSource(conecta.rs);
-            JasperPrint jpTipos = JasperFillManager.fillReport("src\\Relatorios\\clientes.jasper", new HashMap(), relatTipos);
-            JasperViewer jv = new JasperViewer(jpTipos, false);
-            jv.setExtendedState(JasperViewer.MAXIMIZED_BOTH);
-            jv.setTitle("Relatório de Categorias de Veiculos");
-            jv.setVisible(true);
-        } catch (JRException es) {
-            JOptionPane.showMessageDialog(null, "Erro ao execultar o Relatório!\n ERRO :" + es);
-        }
+        rel.imprimeRelClientes();
     }//GEN-LAST:event_jMenuRelClientesActionPerformed
 
     private void jMenuTabPrecosActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuTabPrecosActionPerformed
         // TODO add your handling code here:
-        try {
-            conecta.executaSql("SELECT * FROM PRECOS");
-            JRResultSetDataSource relatTipos = new JRResultSetDataSource(conecta.rs);
-            JasperPrint jpTipos = JasperFillManager.fillReport("src\\Relatorios\\tabelaPrecos.jasper", new HashMap(), relatTipos);
-            JasperViewer jv = new JasperViewer(jpTipos, false);
-            jv.setExtendedState(JasperViewer.MAXIMIZED_BOTH);
-            jv.setTitle("Relatório de Categorias de Veiculos");
-            jv.setVisible(true);
-        } catch (JRException es) {
-            JOptionPane.showMessageDialog(null, "Erro ao execultar o Relatório!\n ERRO :" + es);
-        }
+        rel.imprimeTabelaPrecos();
     }//GEN-LAST:event_jMenuTabPrecosActionPerformed
 
     /**
